ea620157d7007d68513b677c82ed01b1079a5459
RafaGms <faelgomes2003@gmail.com>
2023-04-11 09:30:26 -0300

    third commit

--- src/app/components/conversor/conversor.component.ts
+++ src/app/components/conversor/conversor.component.ts
@@ -13,81 +13,77 @@ export class ConversorComponent {
  //numeros digitados
  primeiroNumero!: number;
  segundoNumero!: number;
+ valorCambio!:number
 
  mensagemError: string = '';
 
 
+ verificacao1() {
+   const numeroConvertido1: number = parseInt(this.selectedOption1); //Converte a strig para numero
+   const numeroConvertido2: number = parseInt(this.selectedOption2); //Converte a strig para numero
+   //verificar se foi selecionado alguma medida
+   if (!isNaN(numeroConvertido1) && !isNaN(numeroConvertido2)) {
+     //if com todas as possibilidade do primeiro imput
+     if (numeroConvertido1 == 1 && numeroConvertido2 == 1) {
+       this.segundoNumero = this.primeiroNumero;
+     } else if (numeroConvertido1 == 1 && numeroConvertido2 == 2) {
+       this.segundoNumero = this.primeiroNumero / this.valorCambio;
+     } else if (numeroConvertido1 == 1 && numeroConvertido2 == 3) {
+       this.segundoNumero = this.primeiroNumero * this.valorCambio;
+     } else if (numeroConvertido1 == 2 && numeroConvertido2 == 1) {
+       this.segundoNumero = this.primeiroNumero * this.valorCambio;
+     } else if (numeroConvertido1 == 2 && numeroConvertido2 == 2) {
+       this.segundoNumero = this.primeiroNumero;
+     } else if (numeroConvertido1 == 2 && numeroConvertido2 == 3) {
+       this.segundoNumero = this.primeiroNumero * this.valorCambio;
+     } else if (numeroConvertido1 == 3 && numeroConvertido2 == 1) {
+       this.segundoNumero = this.primeiroNumero * this.valorCambio;
+     } else if (numeroConvertido1 == 3 && numeroConvertido2 == 2) {
+       this.segundoNumero = this.primeiroNumero * this.valorCambio;
+     } else if (numeroConvertido1 == 3 && numeroConvertido2 == 3) {
+       this.segundoNumero = this.primeiroNumero * this.valorCambio;
+     } else {
+       this.setMesagemError('Operação invalida');
+     }
+   } else {
+     this.setMesagemError('Selecione a moeda dos dois campos!');
+   }
+ }
+ verificacao2() {
+   const numeroConvertido1: number = parseInt(this.selectedOption1); //Converte a strig para numero
+   const numeroConvertido2: number = parseInt(this.selectedOption2); //Converte a strig para numero
+   //verificar se foi selecionado alguma medida
+   if (!isNaN(numeroConvertido1) && !isNaN(numeroConvertido2)) {
+     //if com todas as possibilidade do primeiro imput
+     if (numeroConvertido1 == 1 && numeroConvertido2 == 1) {
+       this.primeiroNumero= this.segundoNumero;
+     } else if (numeroConvertido1 == 1 && numeroConvertido2 == 2) {
+       this.primeiroNumero = this.segundoNumero * this.valorCambio;
+     } else if (numeroConvertido1 == 1 && numeroConvertido2 == 3) {
+       this.primeiroNumero = this.segundoNumero * this.valorCambio;
+     } else if (numeroConvertido1 == 2 && numeroConvertido2 == 1) {
+       this.primeiroNumero = this.segundoNumero * this.valorCambio;
+     } else if (numeroConvertido1 == 2 && numeroConvertido2 == 2) {
+       this.primeiroNumero = this.segundoNumero;
+     } else if (numeroConvertido1 == 2 && numeroConvertido2 == 3) {
+       this.primeiroNumero = this.segundoNumero * this.valorCambio;
+     } else if (numeroConvertido1 == 3 && numeroConvertido2 == 1) {
+       this.primeiroNumero = this.segundoNumero * this.valorCambio;
+     } else if (numeroConvertido1 == 3 && numeroConvertido2 == 2) {
+       this.primeiroNumero = this.segundoNumero * this.valorCambio;
+     } else if (numeroConvertido1 == 3 && numeroConvertido2 == 3) {
+       this.primeiroNumero = this.segundoNumero * this.valorCambio;
+     } else {
+       this.setMesagemError('Operação invalida');
+     }
+   } else {
+     this.setMesagemError('Selecione a medida dos dois campos!');
+   }
+ }
 
+ verificacao3(){
 
-
-
-
-
-
- 
-
-//  verificacao1() {
-//    const numeroConvertido1: number = parseInt(this.selectedOption1); //Converte a strig para numero
-//    const numeroConvertido2: number = parseInt(this.selectedOption2); //Converte a strig para numero
-//    //verificar se foi selecionado alguma medida
-//    if (!isNaN(numeroConvertido1) && !isNaN(numeroConvertido2)) {
-//      //if com todas as possibilidade do primeiro imput
-//      if (numeroConvertido1 == 1 && numeroConvertido2 == 1) {
-//        this.segundoNumero = this.primeiroNumero;
-//      } else if (numeroConvertido1 == 1 && numeroConvertido2 == 2) {
-//        this.segundoNumero = this.primeiroNumero / 1000;
-//      } else if (numeroConvertido1 == 1 && numeroConvertido2 == 3) {
-//        this.segundoNumero = this.primeiroNumero * 100;
-//      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 1) {
-//        this.segundoNumero = this.primeiroNumero * 1000;
-//      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 2) {
-//        this.segundoNumero = this.primeiroNumero;
-//      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 3) {
-//        this.segundoNumero = this.primeiroNumero * 100000;
-//      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 1) {
-//        this.segundoNumero = this.primeiroNumero / 100;
-//      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 2) {
-//        this.segundoNumero = this.primeiroNumero / 100000;
-//      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 3) {
-//        this.segundoNumero = this.primeiroNumero * 100000;
-//      } else {
-//        this.setMesagemError('Operação invalida');
-//      }
-//    } else {
-//      this.setMesagemError('Selecione a medida dos dois campos!');
-//    }
-//  }
-//  verificacao2() {
-//    const numeroConvertido1: number = parseInt(this.selectedOption1); //Converte a strig para numero
-//    const numeroConvertido2: number = parseInt(this.selectedOption2); //Converte a strig para numero
-//    //verificar se foi selecionado alguma medida
-//    if (!isNaN(numeroConvertido1) && !isNaN(numeroConvertido2)) {
-//      //if com todas as possibilidade do primeiro imput
-//      if (numeroConvertido1 == 1 && numeroConvertido2 == 1) {
-//        this.primeiroNumero= this.segundoNumero;
-//      } else if (numeroConvertido1 == 1 && numeroConvertido2 == 2) {
-//        this.primeiroNumero = this.segundoNumero / 1000;
-//      } else if (numeroConvertido1 == 1 && numeroConvertido2 == 3) {
-//        this.primeiroNumero = this.segundoNumero * 100;
-//      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 1) {
-//        this.primeiroNumero = this.segundoNumero * 1000;
-//      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 2) {
-//        this.primeiroNumero = this.segundoNumero;
-//      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 3) {
-//        this.primeiroNumero = this.segundoNumero * 100000;
-//      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 1) {
-//        this.primeiroNumero = this.segundoNumero / 100;
-//      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 2) {
-//        this.primeiroNumero = this.segundoNumero / 100000;
-//      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 3) {
-//        this.primeiroNumero = this.segundoNumero * 100000;
-//      } else {
-//        this.setMesagemError('Operação invalida');
-//      }
-//    } else {
-//      this.setMesagemError('Selecione a medida dos dois campos!');
-//    }
-//  }
+ }
 
  //função para mostrar mensagem de erro
  setMesagemError(mensagem: string) {
